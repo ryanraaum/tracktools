badab6c8aa10293b06baf7949b7f16a9381bfe2d
Ryan Raaum <github@raaum.org>
2025-06-15 19:54:25 -0400

    add cerealization documentation and export the functions

--- R/serialization.R
+++ R/serialization.R
@@ -1,8 +1,26 @@
 
+#' Serialize a complex object into a compressed character string
+#'
+#' @param object Any object
+#'
+#' @returns A character string
+#' @export
+#'
+#' @examples
+#' cerealize(mtcars)
 cerealize <- function(object) {
   paste0(memCompress(serialize(object, NULL), "bzip2"), collapse="")
 }
 
+#' De-serialize a complex object from a compressed character string
+#'
+#' @param cereal A character string of a serialized object
+#'
+#' @returns An R object
+#' @export
+#'
+#' @examples
+#' decerealize(cerealize(mtcars))
 decerealize <- function(cereal) {
   flakes_bz <- substring(cereal, seq(1,nchar(cereal),2), seq(2,nchar(cereal),2))
   flakes_raw <- flakes_bz |> strtoi(16L) |> as.raw()
